200e3379806a43d00d3faef5ca2e8081574849e8
Sergey Protko <fesors@gmail.com>
2016-05-15 20:41:14 +0300

    Add toplevel parser

---
+++ src/RamlParser.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Fesor\RAML;
+
+use Symfony\Component\Yaml\Parser as YamlParser;
+
+class RamlParser
+{
+    private $yamlParser;
+
+    /**
+     * RamlParser constructor.
+     * @param $yamlParser
+     */
+    public function __construct(YamlParser $yamlParser)
+    {
+        $this->yamlParser = $yamlParser;
+    }
+
+    public function parse($raml, $asFragment = null)
+    {
+        $metadata = $this->retreaveMetadata($raml);
+
+        $this->validateMetadata($metadata, $asFragment);
+
+        $yaml = $this->yamlParser->parse($raml);
+        $yaml['_metadata'] = $metadata;
+    }
+
+    private function retreaveMetadata($raml)
+    {
+        if (preg_match('/^#%RAML[\t ]+(\d+\.\d+)([\t ]+(\w+))?\s*/', $raml, $matches)) {
+            return [
+                'version' => (float) $matches[1],
+                'fragment' => isset($matches[3]) ? $matches[3] : null
+            ];
+        }
+
+        return [];
+    }
+
+    private function validateMetadata($metadata, $expectedFragmentType)
+    {
+        if (empty($metadata)) {
+            return;
+        }
+
+        if ($metadata['version'] > 1.0) {
+            throw new \RuntimeException(sprintf(
+                'This parser doesn\'t support this version or RAML specification',
+                $metadata['version']
+            ));
+        }
+
+        if (!isset($metadata['fragment'])) {
+            return;
+        }
+
+        if (!in_array($metadata['fragment'], [
+            'DocumentationItem', 'DataType', 'NamedExample', 'ResourceType', 'Trait',
+            'AnnotationTypeDeclaration', 'Library', 'Overlay', 'Extension', 'SecurityScheme'
+        ])) {
+            throw new \RuntimeException(sprintf(
+                'Unknown RAML fragment "%s"',
+                $metadata['fragment']
+            ));
+        }
+
+        if ($expectedFragmentType && $expectedFragmentType !== $metadata['fragment']) {
+            throw new \RuntimeException(sprintf(
+                'Expected to get "%s" RAML fragment, "%s" given',
+                $expectedFragmentType,
+                $metadata['fragment']
+            ));
+        }
+    }
+}
---
+++ tests/spec/RamlParserSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace spec\Fesor\RAML;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Yaml\Parser;
+
+class RamlParserSpec extends ObjectBehavior
+{
+    private $yamlParserMock;
+
+    function let(Parser $parser)
+    {
+        $this->yamlParserMock = $parser;
+        $this->beConstructedWith($parser);
+    }
+
+    function it_verifies_raml_metadata()
+    {
+        $this->yaml();
+        $this->shouldNotThrow()->duringParse('#%RAML 1.0');
+        $this->shouldNotThrow()->duringParse('#%RAML 0.8');
+        $this->shouldThrow()->duringParse('#%RAML 1.1');
+        $this->shouldNotThrow()->duringParse('#%RAML 1.0 DocumentationItem', 'DocumentationItem');
+        $this->shouldThrow()->duringParse('#%RAML 1.0 DocumentationItem', 'Library');
+        $this->shouldThrow()->duringParse('#%RAML 1.0 FooBar');
+        $this->shouldNotThrow()->duringParse("\n#%RAML 1.0 FooBar");
+    }
+
+    private function yaml(array $raml = [])
+    {
+        $this->yamlParserMock->parse(Argument::any())->willReturn(
+            array_replace(['title' => 'My API'], $raml)
+        )->shouldBeCalled();
+    }
+}
