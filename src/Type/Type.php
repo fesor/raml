ba90840477f0d1e61ba785d8b5aac62c1257d4fb
Sergey Protko <fesors@gmail.com>
2016-05-22 02:44:08 +0300

    Implement object inheritance

--- src/Type/Type.php
+++ src/Type/Type.php
@@ -44,9 +44,53 @@ abstract class Type
     public function extend(array $facets)
     {
         $extendedType = clone $this;
-        $extendedType->facets = array_replace($extendedType->facets, $facets);
+        $extendedType->facets = self::deepMerge($extendedType->facets, $facets);
         $extendedType->baseType = $this;
 
         return $extendedType;
+    }
+
+    public static function extendFromMultipleTypes(array $facets, array $types)
+    {
+        if (self::isMultipleInheritanceSupported($types)) {
+            throw new \RuntimeException('Multiple inheritance is not supported for this combination of types');
+        }
+
+        $className = get_class(current($types));
+        $subtype = new $className($facets);
+        $subtype->baseType = $types;
+        foreach ($types as $type) {
+            $subtype->facets = self::deepMerge($subtype->facets, $type->facets);
+        }
+
+        return $subtype;
+    }
+
+    private static function isMultipleInheritanceSupported(array $types)
+    {
+        if (1 !== count(array_map(function (Type $type) {
+            return get_class($type);
+        }, array_filter($types)))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function deepMerge(array $a, array $b)
+    {
+        $intersection = array_intersect_key($b, $a);
+        $result = array_replace($a, $b);
+        foreach ($intersection as $key => $value) {
+            if (!is_array($value)) continue;
+
+            if (array_keys($value) === range(0, count($value))) {
+                $result[$key] = array_merge($a[$key], $b[$key]);
+            } else {
+                $result[$key] = self::deepMerge($a[$key], $b[$key]);
+            }
+        }
+
+        return $result;
     }
 }
--- tests/spec/Type/ObjectTypeSpec.php
+++ tests/spec/Type/ObjectTypeSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Fesor\RAML\Type;
 
+use Fesor\RAML\Type\ObjectType;
 use Fesor\RAML\Type\PropertyItem;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -21,5 +22,27 @@ class ObjectTypeSpec extends TypeObjectBehaviour
     function it_returns_list_of_required_properties()
     {
         $this->required()->shouldBeLike(['foo']);
+    }
+
+    function it_allows_to_extend_with_additional_properties()
+    {
+        $this->extend([
+            'properties' => [
+                'buz' => new PropertyItem(null, true)
+            ]
+        ])->shouldReturnExtendedType(function (ObjectType $objectType) {
+            return $objectType->required() == ['foo', 'buz'];
+        });
+    }
+
+    function it_allows_to_make_property_optional()
+    {
+        $this->extend([
+            'properties' => [
+                'foo' => new PropertyItem(null, false)
+            ]
+        ])->shouldReturnExtendedType(function (ObjectType $objectType) {
+            return $objectType->required() == [];
+        });
     }
 }
